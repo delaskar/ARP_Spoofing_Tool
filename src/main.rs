03d3e5265ab6705c163d8c18a8d49488e6470abe
Delaskar <delaskarcai@gmail.com>
2023-10-29 23:20:28 +0200

    Update: new write_arp_data_to_directory and write_data_to_file

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,36 @@ use pnet::datalink::{self, Channel, NetworkInterface};
 use pnet::packet::arp::{ArpOperations, ArpPacket};
 use pnet::packet::ethernet::{self, EtherTypes};
 use pnet::packet::Packet;
+use serde_json;
+use std::collections::HashMap;
+use std::fs::{self, OpenOptions};
+use std::io::{self, Write};
 use std::time::{Instant, SystemTime, UNIX_EPOCH};
 use time::OffsetDateTime;
 
-// Definición de la estructura NetworkConfig
-struct NetworkConfig {
-    default_interface: Option<NetworkInterface>,
+// Defining the NetworkConfig structure
+pub struct NetworkConfig {
+    pub default_interface: Option<NetworkInterface>,
+    pub arp_request_data: HashMap<String, String>,
+    pub arp_response_data: HashMap<String, String>,
 }
 
 impl NetworkConfig {
     fn new() -> NetworkConfig {
-        // Obtener la interfaz de red predeterminada
         let default_interface = NetworkConfig::find_default_interface();
-        NetworkConfig { default_interface }
+        let arp_request_data = HashMap::new();
+        let arp_response_data = HashMap::new();
+        NetworkConfig {
+            default_interface,
+            arp_request_data,
+            arp_response_data,
+        }
     }
 
     fn find_default_interface() -> Option<NetworkInterface> {
-        // Obtener la lista de interfaces de red
+        // Get list of network interfaces
         let all_interfaces = datalink::interfaces();
-        // Encontrar la interfaz predeterminada
+        // Find the default interface
         let default_interface = all_interfaces
             .iter()
             .find(|iface| iface.is_up() && !iface.is_loopback() && !iface.ips.is_empty())
@@ -28,11 +39,11 @@ impl NetworkConfig {
         default_interface
     }
 
-    fn capture_arp_packets(&self) {
+    pub fn capture_arp_packets(&mut self) {
         match &self.default_interface {
             Some(interface) => {
                 let (_, mut package_receiver) = NetworkConfig::open_data_channel(&interface);
-                NetworkConfig::process_arp_packets(&mut package_receiver);
+                self.process_arp_packets(&mut package_receiver);
             }
             None => {
                 eprintln!("No suitable default interface found in config.");
@@ -55,7 +66,7 @@ impl NetworkConfig {
         }
     }
 
-    fn process_arp_packets(package_receiver: &mut Box<dyn datalink::DataLinkReceiver>) {
+    fn process_arp_packets(&mut self, package_receiver: &mut Box<dyn datalink::DataLinkReceiver>) {
         // Capture ARP packages in the local network
         loop {
             match package_receiver.next() {
@@ -63,7 +74,7 @@ impl NetworkConfig {
                     let ethernet = ethernet::EthernetPacket::new(packet);
                     if let Some(eth_pkt) = ethernet {
                         if eth_pkt.get_ethertype() == EtherTypes::Arp {
-                            NetworkConfig::handle_arp_packet(&eth_pkt);
+                            self.handle_arp_packet(&eth_pkt);
                         }
                     } else {
                         eprintln!("Issues with Ethernet Packet.");
@@ -74,7 +85,7 @@ impl NetworkConfig {
         }
     }
 
-    fn handle_arp_packet(eth_pkt: &ethernet::EthernetPacket) {
+    fn handle_arp_packet(&mut self, eth_pkt: &ethernet::EthernetPacket) {
         let start_time = Instant::now();
 
         let arp_packet = ArpPacket::new(eth_pkt.payload());
@@ -102,6 +113,28 @@ impl NetworkConfig {
                         println!("  Target IP Address: {}", arp.get_target_proto_addr());
                         println!("  Target MAC Address: {}", arp.get_target_hw_addr());
                         println!("  Protocol Type: {}", arp.get_protocol_type());
+
+                        let data = format!(
+                            "ARP Request ==> (Date: {:?} Time: {:?}, Elapsed Time: {:?}):\n\
+                             Sender IP Address: {}\n\
+                             Sender MAC Address: {}\n\
+                             Target IP Address: {}\n\
+                             Target MAC Address: {}\n\
+                             Protocol Type: {}\n",
+                            formatted_time.date(),
+                            formatted_time.time(),
+                            elapsed_time,
+                            arp.get_sender_proto_addr(),
+                            arp.get_sender_hw_addr(),
+                            arp.get_target_proto_addr(),
+                            arp.get_target_hw_addr(),
+                            arp.get_protocol_type()
+                        );
+
+                        self.arp_request_data.insert(
+                            String::from("ARP Request"),
+                            serde_json::to_string_pretty(&data).unwrap(),
+                        );
                     }
                     ArpOperations::Reply => {
                         println!(
@@ -112,6 +145,22 @@ impl NetworkConfig {
                         );
                         println!("  Sender IP Address: {}", arp.get_sender_proto_addr());
                         println!("  Sender MAC Address: {}", arp.get_sender_hw_addr());
+
+                        let data = format!(
+                            "ARP Response (Date: {:?} Time: {:?}, Elapsed Time: {:?}):\n\
+                             Sender IP Address: {}\n\
+                             Sender MAC Address: {}\n",
+                            formatted_time.date(),
+                            formatted_time.time(),
+                            elapsed_time,
+                            arp.get_sender_proto_addr(),
+                            arp.get_sender_hw_addr()
+                        );
+
+                        self.arp_response_data.insert(
+                            String::from("ARP Response"),
+                            serde_json::to_string_pretty(&data).unwrap(),
+                        );
                     }
                     _ => {
                         eprintln!("Unknown ARP operation.");
@@ -124,9 +173,48 @@ impl NetworkConfig {
             eprintln!("Issues with ARP Packet.");
         }
     }
+
+    pub fn write_arp_data_to_directory(&self, directory: &str) -> io::Result<()> {
+        // Create the folder if it does not exist
+        fs::create_dir_all(directory)?;
+
+        // Full paths of output files
+        let arp_request_file = format!("{}/arp_request.json", directory);
+        let arp_response_file = format!("{}/arp_response.json", directory);
+
+        // Write data to files
+        self.write_data_to_file(&arp_request_file, &self.arp_request_data)?;
+        self.write_data_to_file(&arp_response_file, &self.arp_response_data)?;
+
+        Ok(())
+    }
+
+    fn write_data_to_file(
+        &self,
+        file_name: &str,
+        data: &HashMap<String, String>,
+    ) -> io::Result<()> {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(file_name)?;
+
+        for (_, packet_data) in data {
+            file.write_all(packet_data.as_bytes())?;
+            file.write_all(b"\n")?;
+        }
+        Ok(())
+    }
 }
 
 fn main() {
-    let network_config = NetworkConfig::new();
+    let mut network_config = NetworkConfig::new();
     network_config.capture_arp_packets();
+
+    // Output folder name
+    let output_directory = "output_folder_arp_capture";
+    network_config
+        .write_arp_data_to_directory(output_directory)
+        .unwrap();
 }
