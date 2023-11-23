cfae3e796b8a358430a55743e0dbe1e9414cd58b
Delaskar <delaskarcai@gmail.com>
2023-11-23 11:35:57 +0200

    Update: Finish Module: get_victim_data but with some issues

--- src/arp_spoofing_modules/get_victim_data.rs
+++ src/arp_spoofing_modules/get_victim_data.rs
@@ -15,14 +15,18 @@ enum IPError {
     InvalidFormat,
     OutOfRange,
     IncorrectNumberOfOctets,
+    NoInterfaceExtracted,
 }
 
 impl fmt::Display for IPError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            IPError::InvalidFormat => write!(f, "Error: Invalid IPv4 address format."),
-            IPError::OutOfRange => write!(f, "Error: Octet values out of range."),
-            IPError::IncorrectNumberOfOctets => write!(f, "Error: Incorrect number of octets"),
+            IPError::InvalidFormat => writeln!(f, "Error: Invalid IPv4 address format."),
+            IPError::OutOfRange => writeln!(f, "Error: Octet values out of range."),
+            IPError::IncorrectNumberOfOctets => writeln!(f, "Error: Incorrect number of octets"),
+            IPError::NoInterfaceExtracted => {
+                writeln!(f, "Error while finding the default interface.")
+            }
         }
     }
 }
@@ -84,10 +88,58 @@ fn verify_ip_address(ip_address: String) -> Result<Vec<u8>, IPError> {
     Ok(ip_octets)
 }
 
-pub fn get_victim_mac_address() {
-    let sender_mac = MacAddr::new(0x00, 0x0c, 0x29, 0xb8, 0x13, 0xc6);
-    let sender_ip = Ipv4Addr::new(192, 168, 1, 166);
-    let target_ip = Ipv4Addr::new(192, 168, 1, 131);
+fn network_info() -> Result<(IpAddr, MacAddr), IPError> {
+    let interfaces = datalink::interfaces();
+
+    let default_interface = interfaces
+        .iter()
+        .find(|e| e.is_up() && !e.is_loopback() && !e.ips.is_empty());
+
+    match default_interface {
+        Some(interface) => {
+            let default_mac = match interface.mac {
+                Some(mac) => mac,
+                None => MacAddr::zero(),
+            };
+            let default_ip = interface.ips[0].ip();
+
+            Ok((default_ip, default_mac))
+        }
+        None => Err(IPError::NoInterfaceExtracted),
+    }
+}
+
+fn ip_and_mac_default() -> (Ipv4Addr, MacAddr) {
+    let net_data = match network_info() {
+        Ok(data) => data,
+        Err(_) => {
+            // Default value or an example of IP and MAC address in case of error
+            (IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), MacAddr::zero())
+        }
+    };
+
+    let (ip, mac) = net_data;
+    let ipv4_address: Ipv4Addr = match ip {
+        IpAddr::V4(addr) => addr,
+        _ => Ipv4Addr::new(0, 0, 0, 0), // Default value if the IP is not V4 type
+    };
+
+    (ipv4_address, mac)
+}
+
+pub fn get_victim_mac_address(ip_victim: String) {
+    // Default Host guest details
+    let (ip, mac) = ip_and_mac_default();
+
+    // Victim IPv4
+    let ip_victim = verify_ip_address(ip_victim);
+
+    let sender_mac = mac;
+    let sender_ip = ip;
+    let target_ip = match ip_victim {
+        Ok(ip) => Ipv4Addr::new(ip[0], ip[1], ip[2], ip[3]),
+        _ => Ipv4Addr::LOCALHOST,
+    };
 
     let arp_packet = create_arp_packet(sender_mac, sender_ip, target_ip);
     let mut ethernet_buffer = [0u8; 42];
@@ -118,11 +170,11 @@ pub fn get_victim_mac_address() {
             Ok(packet) => {
                 let ethernet = EthernetPacket::new(packet).unwrap();
                 if ethernet.get_destination() == sender_mac {
-                    println!("Received Ethernet packet: {:?}", ethernet);
+                    println!("Received Ethernet packet: {:?}", ethernet.get_source());
                     break;
                 }
             }
-            _ => eprint!("Packet NOT received!"),
+            _ => eprintln!("Packet NOT received!"),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod arp_spoofing_modules;
+
 pub use crate::arp_spoofing_modules::arp_capture;
 pub use crate::arp_spoofing_modules::get_victim_data;
 
 fn main() {
+    let victim_ip_address = String::from("192.168.1.131");
+
     // Test get_victim_mac_address
-    get_victim_data::get_victim_mac_address();
+    get_victim_data::get_victim_mac_address(victim_ip_address);
 
     let mut network_config = arp_capture::NetworkConfig::new();
     network_config.capture_arp_packets();
